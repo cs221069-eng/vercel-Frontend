915d9737214ff369af30e61bd45d5c59d7a8078f
Moiz Siddiqui <cs221069@dsu.edu.pk>
2026-03-04 14:46:11 +0500

    adding final

--- src/component/LoginPage.jsx
+++ src/component/LoginPage.jsx
@@ -18,7 +18,7 @@ function LoginPage() {
 
     try {
       const response = await axios.post(
-        `${API_BASE_URL}/api/auth/login/9165`,
+        `https://vercel-backend-w7h5.vercel.app/api/auth/login/9165`,
         { email, password },
         { withCredentials: true }
       );
